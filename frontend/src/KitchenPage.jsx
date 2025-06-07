5a1631a6006261172ecd5c45b30562b72a2851eb
Abu Roman <76618493+seeroman@users.noreply.github.com>
2025-06-07 19:12:27 +0300

    Updated KitchenPage.jsx

--- frontend/src/KitchenPage.jsx
+++ frontend/src/KitchenPage.jsx
@@ -160,7 +160,7 @@ const KitchenPage = () => {
 
   const renderOrderList = (orders, isPending = false) => {
     return (
-      <div className="grid gap-7 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+      <div className="grid gap-7 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {orders.map((order) => (
           <div
             key={order.id}
